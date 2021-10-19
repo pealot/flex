b21b3e27babda3aef078e15d292c84f24bba7dca
pealot <92382759+pealot@users.noreply.github.com>
2021-10-19 11:21:14 +0200

    Add files via upload

--- src/components/TermsOfService/TermsOfService.js
+++ src/components/TermsOfService/TermsOfService.js
@@ -11,51 +11,312 @@ const TermsOfService = props => {
   // prettier-ignore
   return (
     <div className={classes}>
-      <p className={css.lastUpdated}>Last updated: October 30, 2017</p>
+      <p className={css.lastUpdated}>Dernère mise à jour : 19 octobre 2021</p>
 
       <p>
-        Thank you for using Saunatime! Ut enim ad minim veniam, quis nostrud exercitation ullamco
-        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        VUE D’ENSEMBLE
       </p>
 
-      <h2>1 Lorem ipsum dolor sit amet</h2>
+      <br>
+
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        Ce site web est exploité par Pealot. Sur ce site, les termes "nous", "notre" et "nos" font référence 
+        à Pealot. Pealot propose ce site web, y compris toutes les informations, tous les outils et tous les 
+        services qui y sont disponibles pour vous, l’utilisateur, sous réserve de votre acceptation de 
+        l’ensemble des modalités, conditions, politiques et avis énoncés ici.
       </p>
 
-      <h2>2 Sed ut perspiciatis unde</h2>
       <p>
-        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
-        laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi
-        architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-        aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
-        voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
-        consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et
-        dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
-        exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi
-        consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil
-        molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+      En visitant ce site et/ou en achetant un de nos produits et services, vous vous engagez dans notre 
+      "Service" et acceptez d’être lié par les modalités suivantes ("Conditions Générales de Service", 
+        "Conditions Générales de Service et d’Utilisation", "Conditions Générales", "Conditions"), y compris 
+      les modalités, conditions et politiques additionnelles auxquelles il est fait référence ici et/ou 
+      accessibles par hyperlien. Ces Conditions Générales de Service et d’Utilisation s’appliquent à tous 
+      les utilisateurs de ce site, incluant mais ne se limitant pas, aux utilisateurs qui naviguent sur le 
+      site, qui sont des vendeurs, des clients, des marchands, et/ou des contributeurs de contenu.
       </p>
 
-      <h2>3 At vero eos et accusamus</h2>
       <p>
-        At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium
-        voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati
-        cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id
-        est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam
-        libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod
-        maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.
-        Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut
-        et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a
-        sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis
-        doloribus asperiores repellat
+      Veuillez lire attentivement ces Conditions Générales de Service et d’Utilisation avant d’accéder 
+      à ou d’utiliser notre site web. En accédant à ou en utilisant une quelconque partie de ce site, 
+      vous acceptez d’être lié par ces Conditions Générales de Service et d’Utilisation. Si vous n’acceptez 
+      pas toutes les modalités et toutes les Conditions de cet accord, alors vous ne devez pas accéder au 
+      site web ni utiliser les services qui y sont proposés. Si ces Conditions Générales de Service et 
+      d’Utilisation sont considérées comme une offre, l’acceptation se limite expressément à ces Conditions 
+      Générales de Service et d’Utilisation.
       </p>
+
+      <p>
+      Toutes les nouvelles fonctionnalités et tous les nouveaux outils qui seront ajoutés ultérieurement à 
+      ce site seront également assujettis à ces Conditions Générales de Service et d’Utilisation. Vous 
+      pouvez consulter la version la plus récente des Conditions Générales de Service et d’Utilisation 
+      à tout moment sur cette page. Nous nous réservons le droit de mettre à jour, de changer ou de remplacer 
+      toute partie de ces Conditions Générales de Service et d’Utilisation en publiant les mises à jour et/ou 
+      les modifications sur notre site web. Il vous incombe de consulter cette page régulièrement pour vérifier 
+      si des modifications ont été apportées. Votre utilisation continue du site web ou votre accès à celui-ci 
+      après la publication de toute modification constitue une acception de votre part de ces modifications.
+      </p>
+
+      <p>
+      Notre boutique est hébergée sur Sharetribe Inc. Ils nous fournissent la plate-forme e-commerce qui 
+      nous permet de vous vendre nos produits et services.
+      </p>
+
+
+
+      <h2>ARTICLE 1 – CONDITIONS D’UTILISATION DE NOTRE BOUTIQUE EN LIGNE</h2>
+      <p>
+        En acceptant ces Conditions Générales de Service et d’Utilisation, vous déclarez que vous 
+        avez atteint l’âge de la majorité dans votre pays, État ou province de résidence, et que 
+        vous nous avez donné votre consentement pour permettre à toute personne d’âge mineur à 
+        votre charge d’utiliser ce site web.
+      </p>
+
+      <p>
+        L’utilisation de nos produits et services à toute fin illégale ou non autorisée est interdite, 
+        et vous ne devez pas non plus, dans le cadre de l’utilisation du Service, violer les lois de 
+        votre juridiction (incluant mais ne se limitant pas aux lois relatives aux droits d’auteur).
+      </p>
+
+      <p>
+        Vous ne devez pas transmettre de ver informatique ("worm"), de virus ou tout autre code de 
+        nature destructive.
+      </p>
+
+      <p>
+        Toute infraction ou violation des présentes Conditions Générales de Service et d’Utilisation 
+        entraînera la résiliation immédiate de vos Services.
+      </p>
+
+      <h2>ARTICLE 2 – CONDITIONS GÉNÉRALES</h2>
+
+      <p>
+        
+      </p>
+
+      <p>
+        
+      </p>
+
+      <p>
+        
+      </p>
+
+      <p>
+        
+      </p>
+
+      <h2>ARTICLE 3 – EXACTITUDE, EXHAUSTIVITÉ ET ACTUALITÉ DES INFORMATIONS</h2>
+      <p>
+        
+      </p>
+
+      <p>
+        
+      </p>
+
+      <h2>ARTICLE 4 – MODIFICATIONS APPORTÉES AU SERVICE ET AUX PRIX</h2>
+
+      <p>
+        
+      </p>
+
+      <p>
+        
+      </p>
+
+      <p>
+        
+      </p>
+
+      <h2>ARTICLE 5 – PRODUITS OU SERVICES</h2>
+
+      <p>
+        
+      </p>
+
+      <p>
+        
+      </p>
+
+      <p>
+        
+      </p>
+
+      <p>
+        
+      </p>
+
+      <p>
+        
+      </p>
+
+      <h2>ARTICLE 6 – EXACTITUDE DE LA FACTURATION ET DES INFORMATIONS DE COMPTE</h2>
+
+      <p>
+        
+      </p>
+
+      <p>
+        
+      </p>
+
+      <p>
+        
+      </p>
+
+      <h2>ARTICLE 7 – OUTILS FACULTATIFS</h2>
+
+      <p>
+        
+      </p>
+
+      <p>
+        
+      </p>
+
+      <p>
+        
+      </p>
+
+      <p>
+        
+      </p>
+
+      <h2>ARTICLE 8 – LIENS DE TIERS</h2>
+
+      <p>
+        
+      </p>
+
+      <p>
+        
+      </p>
+
+      <p>
+        
+      </p>
+
+      <h2>ARTICLE 9 – COMMENTAIRES, MESSAGES, SUGGESTIONS ET AUTRES PROPOSITIONS D’UTILISATEURS</h2>
+
+      <p>
+        
+      </p>
+
+      <p>
+        
+      </p>
+
+      <p>
+        
+      </p>
+
+      <h2>ARTICLE 10 – RENSEIGNEMENTS PERSONNELS</h2>
+
+      <p>
+        
+      </p>
+
+      <h2>ARTICLE 11 – ERREURS, INEXACTITUDES ET OMISSIONS</h2>
+
+      <p>
+        
+      </p>
+
+      <p>
+        
+      </p>
+
+      <h2>ARTICLE 12 – UTILISATIONS INTERDITES</h2>
+
+      <p>
+        
+      </p>
+
+      <h2>ARTICLE 13 – EXCLUSION DE GARANTIES ET LIMITATION DE RESPONSABILITÉ</h2>
+
+      <p>
+        
+      </p>
+
+      <p>
+        
+      </p>
+
+      <p>
+        
+      </p>
+
+      <p>
+        
+      </p>
+
+      <p>
+        
+      </p>
+
+      <h2>ARTICLE 14 – INDEMNISATION</h2>
+
+      <p>
+        
+      </p>
+
+      <h2>ARTICLE 15 – DISSOCIABILITÉ</h2>
+
+      <p>
+        
+      </p>
+
+      <h2>ARTICLE 16 – RÉSILIATION</h2>
+
+      <p>
+        
+      </p>
+
+      <p>
+        
+      </p>
+
+      <p>
+        
+      </p>
+
+      <h2>ARTICLE 17 – INTÉGRALITÉ DE L’ACCORD</h2>
+
+      <p>
+        
+      </p>
+
+      <p>
+        
+      </p>
+
+      <p>
+        
+      </p>
+
+      <h2>ARTICLE 18 – LOI APPLICABLE</h2>
+
+      <p>
+        
+      </p>
+
+      <h2>ARTICLE 19 – MODIFICATIONS APPORTÉES AUX CONDITIONS GÉNÉRALES DE SERVICE ET D’UTILISATION</h2>
+
+      <p>
+        
+      </p>
+
+      <p>
+        
+      </p>
+
+      <h2>ARTICLE 20 – COORDONNÉES</h2>
+
+      <p>
+        
+      </p>
+
     </div>
   );
 };
