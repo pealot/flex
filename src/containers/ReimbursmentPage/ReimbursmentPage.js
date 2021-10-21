40459d5bb16e7c598b532002f135bd458fd60573
pealot <92382759+pealot@users.noreply.github.com>
2021-10-21 18:15:34 +0200

    Add files via upload

---
+++ src/containers/ReimbursmentPage/ReimbursmentPage.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { FormattedMessage, injectIntl, intlShape } from '../../util/reactIntl';
+import { isScrollingDisabled } from '../../ducks/UI.duck';
+import { TopbarContainer } from '../../containers';
+import {
+  Page,
+  LayoutSideNavigation,
+  LayoutWrapperMain,
+  LayoutWrapperSideNav,
+  LayoutWrapperTopbar,
+  LayoutWrapperFooter,
+  Footer,
+  Reimbursment,
+} from '../../components';
+import config from '../../config';
+
+import css from './ReimbursmentPage.module.css';
+
+const ReimbursmentPageComponent = props => {
+  const { scrollingDisabled, intl } = props;
+
+  const tabs = [
+    {
+      text: intl.formatMessage({ id: 'ReimbursmentPage.privacyTabTitle' }),
+      selected: false,
+      linkProps: {
+        name: 'PrivacyPolicyPage',
+      },
+    },
+    {
+      text: intl.formatMessage({ id: 'ReimbursmentPage.tosTabTitle' }),
+      selected: false,
+      linkProps: {
+        name: 'TermsOfServicePage',
+      },
+    },
+    {
+      text: intl.formatMessage({ id: 'ReimbursmentPage.reimbursmentTabTitle' }),
+      selected: true,
+      linkProps: {
+        name: 'ReimbursmentPage',
+      },
+    },
+  ];
+  const siteTitle = config.siteTitle;
+  const schemaTitle = intl.formatMessage({ id: 'ReimbursmentPage.schemaTitle' }, { siteTitle });
+  const schema = {
+    '@context': 'http://schema.org',
+    '@type': 'WebPage',
+    name: schemaTitle,
+  };
+  return (
+    <Page title={schemaTitle} scrollingDisabled={scrollingDisabled} schema={schema}>
+      <LayoutSideNavigation>
+        <LayoutWrapperTopbar>
+          <TopbarContainer currentPage="ReimbursmentPage" />
+        </LayoutWrapperTopbar>
+        <LayoutWrapperSideNav tabs={tabs} />
+        <LayoutWrapperMain>
+          <div className={css.content}>
+            <h1 className={css.heading}>
+              <FormattedMessage id="ReimbursmentPage.heading" />
+            </h1>
+            <Reimbursment />
+          </div>
+        </LayoutWrapperMain>
+        <LayoutWrapperFooter>
+          <Footer />
+        </LayoutWrapperFooter>
+      </LayoutSideNavigation>
+    </Page>
+  );
+};
+
+const { bool } = PropTypes;
+
+ReimbursmentPageComponent.propTypes = {
+  scrollingDisabled: bool.isRequired,
+
+  // from injectIntl
+  intl: intlShape.isRequired,
+};
+
+const mapStateToProps = state => {
+  return {
+    scrollingDisabled: isScrollingDisabled(state),
+  };
+};
+
+const ReimbursmentPage = compose(
+  connect(mapStateToProps),
+  injectIntl
+)(ReimbursmentPageComponent);
+
+export default ReimbursmentPage;
