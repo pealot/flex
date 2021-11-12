32a8ef8091eeaa8b0fb337b3d31cc9fb8ccdab11
pealot <92382759+pealot@users.noreply.github.com>
2021-11-12 14:47:19 +0100

    Add files via upload

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -200,6 +200,7 @@ const Footer = props => {
             <NamedLink name="LandingPage" className={css.organizationCopyrightMobile}>
               <FormattedMessage id="Footer.copyright" />
             </NamedLink>
+            <br> </br>
             <div className={css.tosAndPrivacyMobile}>
               <NamedLink name="PrivacyPolicyPage" className={css.termsAndprivacy}>
                 <FormattedMessage id="Footer.privacy" />
